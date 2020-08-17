6b6bbfa4520ea3180898d560d32aa586050499f2
gerayvahidli <54402993+gerayvahidli@users.noreply.github.com>
2020-08-17 17:29:40 +0400

    change titles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Daxil ol') }}</div>
+                <div class="card-header"> Xarici təqaüd programı | {{ __('Daxil ol') }}</div>
 
 
 
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Şifrəmi yenilə') }}</div>
+                <div class="card-header">Xarici təqaüd programı | {{ __('Şifrəmi yenilə') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
