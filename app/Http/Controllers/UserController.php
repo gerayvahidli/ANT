61747e1fa058a248bc95f3e51584b25a27916863
gerayvahidli <54402993+gerayvahidli@users.noreply.github.com>
2020-08-24 12:19:29 +0400

    test feedback form mail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -109,7 +109,7 @@ class UserController extends Controller
             'file' => ($request->has('file')) ? $request->file : null,
             'phone_number' => $request->phone_number
         ];
-        \Mail::to('tis@socar.az')->send(new FromUserToTis($userData));
+        \Mail::to('garay.vahidli@socar.az')->send(new FromUserToTis($userData));
 //		\Mail::to( 'ilkin.fleydanli@socar.az' )->send( new FromUserToTis( $userData ) );
 
         return redirect(route('profile.index'));
