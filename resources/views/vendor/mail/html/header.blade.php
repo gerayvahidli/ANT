59dd5e328097b5250afedb1d73dbffa59cdf8d6d
gerayvahidli <54402993+gerayvahidli@users.noreply.github.com>
2020-08-25 11:10:55 +0400

    change mail header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url.'/XTP' }}">
-            SOCAR Xarici Təqaüd Proqramı - Müraciət
+            SOCAR Xarici Təqaüd Proqramı
         </a>
     </td>
 </tr>
